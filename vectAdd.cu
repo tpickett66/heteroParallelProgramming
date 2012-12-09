78933fe9506cf2a6c49fed19e3440bb1d69cb4db
t.pickett66 <t.pickett66@gmail.com>
2012-12-08 19:16:30 -0600

    implement vector addition function
    Signed-off-by: t.pickett66 <t.pickett66@gmail.com>

--- vectAdd.cu
+++ vectAdd.cu
@@ -1,8 +1,15 @@
 // MP 1
-#include	<wb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <cuda.h>
+#include <cuda_runtime.h>
+//#include	<wb.h>
 
 __global__ void vecAdd(float * in1, float * in2, float * out, int len) {
-    //@@ Insert code to implement vector addition here
+  int idx = threadId.x + blockDim.x * blockIdx.x;
+  if(idx < len) {
+    out[idx] = in1[idx] + in2[idx]
+  }
 }
 
 int main(int argc, char ** argv) {
